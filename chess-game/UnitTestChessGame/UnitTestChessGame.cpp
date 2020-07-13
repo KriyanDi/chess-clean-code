f79b295cff6dbc77d9c803c8163dfeed300e4be7
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-13 14:58:00 +0300

    Added Unit Test Project in the Solution

---
+++ chess-game/UnitTestChessGame/UnitTestChessGame.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestChessGame
+{
+	TEST_CLASS(UnitTestChessGame)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
