b3160bd012c15c8f5aa350c18f42fde8f0e75460
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-15 19:19:00 +0300

    Changed logic in set_figure method

--- chess-game/chess-game/Square.cpp
+++ chess-game/chess-game/Square.cpp
@@ -2,6 +2,14 @@
 
 #include "Square.h"
 
+//Figures
+#include "Pawn.h"
+#include "Rook.h"
+#include "Horse.h"
+#include "Bishop.h"
+#include "Queen.h"
+#include "King.h"
+
 Square::Square()
 {
 	this->figure = nullptr;
@@ -14,7 +22,35 @@ Square::~Square()
 
 void Square::set_figure(Figure figure)
 {
-	this->figure = new Figure(figure);
+	Type type = figure.get_type();
+	Color color = figure.get_color();
+	Position position = figure.get_position();
+	Player owner = figure.get_owner();
+
+	switch (figure.get_type())
+	{
+	case Type::PAWN:
+		this->figure = new Pawn(type, color, position, owner);
+		break;
+	case Type::ROOK:
+		this->figure = new Rook(type, color, position, owner);
+		break;
+	case Type::KNIGHT:
+		this->figure = new Horse(type, color, position, owner);
+		break;
+	case Type::BISHOP:
+		this->figure = new Bishop(type, color, position, owner);
+		break;
+	case Type::QUEEN:
+		this->figure = new Queen(type, color, position, owner);
+		break;
+	case Type::KING:
+		this->figure = new King(type, color, position, owner);
+		break;
+	default:
+		break;
+	}
+
 	if (!this->figure)
 	{
 		std::cout << "Problem allocating memory for figure!\n";
@@ -44,7 +80,6 @@ Figure Square::remove_figure()
 	if (!this->is_empty())
 	{
 		Figure temp_figure(*this->figure);
-		delete this->figure;
 		figure = nullptr;
 		return temp_figure;
 	}
