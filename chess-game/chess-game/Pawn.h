1a9730ef6251f4dfac8bed255fd4e351f147285b
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-16 19:41:09 +0300

    Removed movement methods from Pawn class
    Now all methods are in separate class called ForwardMovement, this class is specialy made for Pawn

--- chess-game/chess-game/Pawn.h
+++ chess-game/chess-game/Pawn.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#include "Figure.h"
+#include "ForwardMovement.h"
 
-class Pawn : virtual public Figure
+class Pawn
+	: virtual public Figure
+	, public ForwardMovement
 {
 public:
 	Pawn(Type& type, Color& color, Position& position, Player& owner);
@@ -12,23 +14,4 @@ public:
 
 public:
 	std::vector<Position> get_all_possible_moves(Game* game) override;
-
-private:
-	void set_one_step_forward_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_one_step_forward(Position& position);
-	Position get_one_step_forward_black_figure();
-	Position get_one_step_forward_white_figure();
-	
-	void set_two_steps_forward_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_two_steps_forward(Position& position);
-	Position get_two_steps_forward_black_figure();
-	Position get_two_steps_forward_white_figure();
-
-	void set_diagonal_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_two_diagonal_moves(Position& left_diagonal, Position& right_diagonal);
-	std::vector<Position> get_two_diagonal_moves_black_figure();
-	std::vector<Position> get_two_diagonal_moves_white_figure();
-
-	bool is_position_empty(Game* game, Position& position);
-	bool has_position_enemy_figure(Game* game, Position& position);
 };	
