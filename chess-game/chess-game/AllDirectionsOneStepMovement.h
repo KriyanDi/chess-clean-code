524857e0a0f8b08c6153260eded62f98f2a69005
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-16 16:00:04 +0300

    Changed methods attribute in AllDirectionsOneStepMovement class
    Now basic methods for movement are accessible from inherited classes

--- chess-game/chess-game/AllDirectionsOneStepMovement.h
+++ chess-game/chess-game/AllDirectionsOneStepMovement.h
@@ -23,41 +23,42 @@ protected:
 	bool is_position_empty(Game* game, Position& position);
 	bool has_position_enemy_figure(Game* game, Position& position);
 
-private:
+	void set_one_step_forward(Position& position, Position current);
+	void set_one_step_backwards(Position& position, Position current);
+	void set_one_step_left(Position& position, Position current);
+	void set_one_step_right(Position& position, Position current);
+	void set_one_step_forward_left(Position& position, Position current);
+	void set_one_step_backwards_left(Position& position, Position current);
+	void set_one_step_forward_right(Position& position, Position current);
+	void set_one_step_backwards_right(Position& position, Position current);
+
+protected:
 	Figure* figure;
 
 private:
 	void set_figure_pointer(Figure* figure);
 
-	void set_one_step_forward(Position& position, Position current);
 	Position get_one_step_forward_black_figure(Position& position);
 	Position get_one_step_forward_white_figure(Position& position);
 
-	void set_one_step_backwards(Position& position, Position current);
 	Position get_one_step_backwards_black_figure(Position& position);
 	Position get_one_step_backwards_white_figure(Position& position);
 
-	void set_one_step_left(Position& position, Position current);
 	Position get_one_step_left_black_figure(Position& position);
 	Position get_one_step_left_white_figure(Position& position);
 
-	void set_one_step_right(Position& position, Position current);
 	Position get_one_step_right_black_figure(Position& position);
 	Position get_one_step_right_white_figure(Position& position);
 
-	void set_one_step_forward_left(Position& position, Position current);
 	Position get_one_step_forward_left_black_figure(Position& position);
 	Position get_one_step_forward_left_white_figure(Position& position);
 
-	void set_one_step_backwards_left(Position& position, Position current);
 	Position get_one_step_backwards_left_black_figure(Position& position);
 	Position get_one_step_backwards_left_white_figure(Position& position);
 
-	void set_one_step_forward_right(Position& position, Position current);
 	Position get_one_step_forward_right_black_figure(Position& position);
 	Position get_one_step_forward_right_white_figure(Position& position);
 
-	void set_one_step_backwards_right(Position& position, Position current);
 	Position get_one_step_backwards_right_black_figure(Position& position);
 	Position get_one_step_backwards_right_white_figure(Position& position);
 };
