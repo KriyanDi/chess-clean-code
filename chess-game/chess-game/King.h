06d956c8e92276adcdc9ec757238e5d142e0b57e
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-16 14:15:14 +0300

    Overrided method get_all_possible_moves in King class

--- chess-game/chess-game/King.h
+++ chess-game/chess-game/King.h
@@ -9,4 +9,54 @@ public:
 	King(const King& other) = delete;
 	King& operator=(const King& other) = delete;
 	virtual ~King();
+
+public:
+	std::vector<Position> get_all_possible_moves(Game* game) override;
+
+protected:
+	void set_one_step_all_directions_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
+
+private:
+	void set_one_step_forward_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
+	void set_one_step_forward(Position& position, Position current);
+	Position get_one_step_forward_black_figure(Position& position);
+	Position get_one_step_forward_white_figure(Position& position);
+
+	void set_one_step_backwards_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
+	void set_one_step_backwards(Position& position, Position current);
+	Position get_one_step_backwards_black_figure(Position& position);
+	Position get_one_step_backwards_white_figure(Position& position);
+
+	void set_one_step_left_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
+	void set_one_step_left(Position& position, Position current);
+	Position get_one_step_left_black_figure(Position& position);
+	Position get_one_step_left_white_figure(Position& position);
+
+	void set_one_step_right_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
+	void set_one_step_right(Position& position, Position current);
+	Position get_one_step_right_black_figure(Position& position);
+	Position get_one_step_right_white_figure(Position& position);
+
+	void set_one_step_forward_left_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
+	void set_one_step_forward_left(Position& position, Position current);
+	Position get_one_step_forward_left_black_figure(Position& position);
+	Position get_one_step_forward_left_white_figure(Position& position);
+
+	void set_one_step_backwards_left_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
+	void set_one_step_backwards_left(Position& position, Position current);
+	Position get_one_step_backwards_left_black_figure(Position& position);
+	Position get_one_step_backwards_left_white_figure(Position& position);
+
+	void set_one_step_forward_right_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
+	void set_one_step_forward_right(Position& position, Position current);
+	Position get_one_step_forward_right_black_figure(Position& position);
+	Position get_one_step_forward_right_white_figure(Position& position);
+
+	void set_one_step_backwards_right_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
+	void set_one_step_backwards_right(Position& position, Position current);
+	Position get_one_step_backwards_right_black_figure(Position& position);
+	Position get_one_step_backwards_right_white_figure(Position& position);
+
+	bool is_position_empty(Game* game, Position& position);
+	bool has_position_enemy_figure(Game* game, Position& position);
 };
