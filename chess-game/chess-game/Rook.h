c9f9fe4a992fbde489290cc21a2dec64252c6db7
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-16 16:02:01 +0300

    Removed movement methods from Rook class
    Took out movement methods and putted them in a separate class. Now Rook inherits HorizontalMovement class

--- chess-game/chess-game/Rook.h
+++ chess-game/chess-game/Rook.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "Figure.h"
+#include "HorizontalMovement.h"
 
-class Rook : virtual public Figure
+class Rook : virtual public Figure, public HorizontalMovement
 {
 public:
 	Rook(Type& type, Color& color, Position& position, Player& owner);
@@ -12,32 +12,4 @@ public:
 
 public:
 	std::vector<Position> get_all_possible_moves(Game* game) override;
-
-protected:
-	void set_vertical_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_horizontal_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-
-private:
-	void set_forward_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_step_forward(Position& position, Position current);
-	Position get_step_forward_black_figure(Position position);
-	Position get_step_forward_white_figure(Position position);
-
-	void set_backwards_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_step_backwards(Position& position, Position current);
-	Position get_step_backwards_black_figure(Position position);
-	Position get_step_backwards_white_figure(Position position);
-
-	void set_left_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_step_left(Position& position, Position current);
-	Position get_step_left_black_figure(Position position);
-	Position get_step_left_white_figure(Position position);
-
-	void set_right_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_step_right(Position& position, Position current);
-	Position get_step_right_black_figure(Position position);
-	Position get_step_right_white_figure(Position position);
-
-	bool is_position_empty(Game* game, Position& position);
-	bool has_position_enemy_figure(Game* game, Position& position);
 };
