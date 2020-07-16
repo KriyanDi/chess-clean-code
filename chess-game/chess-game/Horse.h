0b94ae390f035001d94f8dd20d69730ad669a4f3
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-16 19:02:29 +0300

    Removed movement methods from Horse class
    Now those methods are moved into separate class called ExtendedDiagonalMovement

--- chess-game/chess-game/Horse.h
+++ chess-game/chess-game/Horse.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#include "Figure.h"
+#include "ExtendedDiagonalMovement.h"
 
-class Horse : virtual public Figure
+class Horse 
+	: virtual public Figure
+	, public ExtendedDiagonalMovement
 {
 public:
 	Horse(Type& type, Color& color, Position& position, Player& owner);
@@ -12,28 +14,4 @@ public:
 
 public:
 	std::vector<Position> get_all_possible_moves(Game* game) override;
-
-private:
-	void set_forward_extended_diagonal_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_forward_extended_diagonal(Position& left_extended_diagonal, Position& right_extended_diagonal);
-	std::vector<Position> get_forward_extended_diagonal_moves_black_figure();
-	std::vector<Position> get_forward_extended_diagonal_moves_white_figure();
-
-	void set_backwards_extended_diagonal_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_backwards_extended_diagonal(Position& left_extended_diagonal, Position& right_extended_diagonal);
-	std::vector<Position> get_backwards_extended_diagonal_moves_black_figure();
-	std::vector<Position> get_backwards_extended_diagonal_moves_white_figure();
-
-	void set_left_extended_diagonal_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_left_extended_diagonal(Position& left_extended_diagonal, Position& right_extended_diagonal);
-	std::vector<Position> get_left_extended_diagonal_moves_black_figure();
-	std::vector<Position> get_left_extended_diagonal_moves_white_figure();
-
-	void set_right_extended_diagonal_possible_moves(Game* game, std::vector<Position>& all_possible_moves);
-	void set_right_extended_diagonal(Position& left_extended_diagonal, Position& right_extended_diagonal);
-	std::vector<Position> get_right_extended_diagonal_moves_black_figure();
-	std::vector<Position> get_right_extended_diagonal_moves_white_figure();
-
-	bool is_position_empty(Game* game, Position& position);
-	bool has_position_enemy_figure(Game* game, Position& position);
 };
