7e349dd5755c6d7320109fc69eab73fdcb5e98f4
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-16 12:00:14 +0300

    Overrided method get_all_possible_moves in Queen class

--- chess-game/chess-game/Queen.cpp
+++ chess-game/chess-game/Queen.cpp
@@ -8,3 +8,14 @@ Queen::Queen(Type& type, Color& color, Position& position, Player& owner)
 Queen::~Queen()
 {
 }
+
+std::vector<Position> Queen::get_all_possible_moves(Game* game)
+{
+	std::vector<Position> all_possible_moves;
+	set_horizontal_possible_moves(game, all_possible_moves);
+	set_vertical_possible_moves(game, all_possible_moves);
+	set_right_diagonal_possible_moves(game, all_possible_moves);
+	set_left_diagonal_possible_moves(game, all_possible_moves);
+
+	return all_possible_moves;
+}
--- chess-game/chess-game/Queen.h
+++ chess-game/chess-game/Queen.h
@@ -1,12 +1,17 @@
 #pragma once
 
 #include "Figure.h"
+#include "Rook.h"
+#include "Bishop.h"
 
-class Queen : virtual public Figure
+class Queen : virtual public Figure, public Rook, public Bishop
 {
 public:
 	Queen(Type& type, Color& color, Position& position, Player& owner);
 	Queen(const Queen& other) = delete;
 	Queen& operator=(const Queen& other) = delete;
 	virtual ~Queen();
+
+public:
+	std::vector<Position> get_all_possible_moves(Game* game) override;
 };
