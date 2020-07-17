4bcde72eaeff4adcab4d495f4e002aabdfac897c
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-17 23:17:17 +0300

    Changed some methods attribute from public to private

--- chess-game/chess-game/Board.h
+++ chess-game/chess-game/Board.h
@@ -8,6 +8,12 @@ const int BOARD_WIDTH = 8;
 const int BOARD_FIELD_VIEW_HIGH = 12;
 const int BOARD_FIELD_VIEW_WIDTH = 24;
 
+/// This is Chess board field
+/// 
+/// This class is about setting the board field,
+/// printing the board field on the console. It 
+/// also has the rules of putting the figures in
+/// the correct order on the board field
 class Board
 {
 public:
@@ -20,25 +26,13 @@ public:
 	void set_board_field();
 
 	void set_figure(Position& position, Figure* figure);
+
 	Figure* get_figure(Position& position);
 	void remove_figure(Position& position);
-
 	bool is_empty(Position& position) const;
 
 	void print_board_field_view();
 	void update_board_field_view_center();
-
-	void set_board_field_view();
-	void set_board_field_view_center();
-	void set_board_field_view_outlines();
-	void set_all_chars_with_spaces_board_field_view();
-	void set_top_numbers_board_field_view();
-	void set_bottom_numbers_board_field_view();
-	void set_left_side_numbers_board_field_view();
-	void set_right_side_numbers_board_field_view();
-
-	int convert_row_board_field_view_to_row_board_view_index(int& row);
-	int convert_col_board_field_view_to_col_board_view_index(int& col);
 
 private:
 	Square** board_field;
@@ -52,6 +46,7 @@ private:
 	void place_pawns();
 	void place_black_pawns();
 	void place_white_pawns();
+
 
 	void place_rooks();
 	void place_black_rooks();
@@ -72,4 +67,16 @@ private:
 	void place_kings();
 	void place_black_king();
 	void place_white_king();
+
+	void set_board_field_view();
+	void set_board_field_view_center();
+	void set_board_field_view_outlines();
+	void set_all_chars_with_spaces_board_field_view();
+	void set_top_numbers_board_field_view();
+	void set_bottom_numbers_board_field_view();
+	void set_left_side_numbers_board_field_view();
+	void set_right_side_numbers_board_field_view();
+
+	int convert_row_board_field_view_to_row_board_view_index(int& row);
+	int convert_col_board_field_view_to_col_board_view_index(int& col);
 };
