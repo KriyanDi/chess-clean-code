3192f696483ddecd7c84dc3c2466b7a6499b8496
Kristiyan Dimitrov <kriyandimitrov@gmail.com>
2020-07-18 05:24:51 +0300

    Added Unit test for class Figure

--- chess-game/UnitTestChessGame/UnitTestChessGame.cpp
+++ chess-game/UnitTestChessGame/UnitTestChessGame.cpp
@@ -29,6 +29,75 @@ using namespace std;
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+template<> inline std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString<Type>(const Type& t) 
+{
+	switch (t)
+	{
+	case Type::PAWN:
+		return L"pawn";
+
+	case Type::ROOK:
+		return L"rook";
+
+	case Type::KNIGHT:
+		return L"horse";
+
+	case Type::BISHOP:
+		return L"bishop";
+
+	case Type::QUEEN:
+		return L"queen";
+
+	case Type::KING:
+		return L"king";
+
+	default:
+		break;
+	}
+}
+template<> inline std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString<Color>(const Color& t)
+{
+	switch (t)
+	{
+	case Color::WHITE:
+		return L"white";
+
+	case Color::BLACK:
+		return L"black";
+
+	case Color::NO_COLOR:
+		return L"nocol";
+
+	default:
+		break;
+	}
+}
+template<> inline std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString<Position>(const Position& t)
+{
+	return ToString(t.get_row()) + ToString(t.get_col());
+}
+template<> inline std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString<Player>(const Player& t)
+{
+	switch (t)
+	{
+	case Player::PLAYER_ONE:
+		return L"pl1";
+
+	case Player::PLAYER_TWO:
+		return L"pl2";
+
+	case Player::NO_PLAYER:
+		return L"nopl";
+
+	default:
+		break;
+	}
+}
+template<> inline std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString<Figure>(const Figure& t)
+{
+	return ToString(t.get_type()) + ToString(t.get_color()) + ToString(t.get_position()) + ToString(t.get_owner());
+}
+
 namespace UnitTestChessGame
 {
 	TEST_CLASS(TestPositionClass)
@@ -204,10 +273,258 @@ namespace UnitTestChessGame
 		{
 			Position position(5, 3);
 
-			bool expected = false;
+			bool expected = true;
 			bool actual = position.is_valid_position();
 
 			Assert::AreEqual(expected, actual, L"Should be true, position is valid");
+		}
+	};
+
+	TEST_CLASS(TestFigureClass)
+	{
+		TEST_METHOD(testFigureDefaulConstructorWithGivenTypeColorPositionOwnerTypeEqualsKing)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+
+			Figure figure(type, color, position, owner);
+
+			Type expected = Type::KING;
+			Type actual = figure.get_type();
+
+			Assert::AreEqual(expected, actual, L"Type should be King");
+		}
+
+		TEST_METHOD(testFigureDefaulConstructorWithGivenTypeColorPositionOwnerColorEqualsWhite)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+
+			Figure figure(type, color, position, owner);
+
+			Color expected = Color::WHITE;
+			Color actual = figure.get_color();
+
+			Assert::AreEqual(expected, actual, L"Type should be White");
+		}
+
+		TEST_METHOD(testFigureDefaulConstructorWithGivenTypeColorPositionOwnerPositionEqualsRowFourColumnThree)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position_figure(4, 3);
+			Position position_other(4, 3);
+			Player owner = Player::PLAYER_ONE;
+
+			Figure figure(type, color, position_figure, owner);
+
+			Position expected = position_other;
+			Position actual = figure.get_position();
+
+			Assert::AreEqual(expected, actual, L"Should be true, positions are the same");
+		}
+
+		TEST_METHOD(testFigureDefaulConstructorWithGivenTypeColorPositionOwnerEqualsPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+
+			Figure figure(type, color, position, owner);
+
+			Player expected = Player::PLAYER_ONE;
+			Player actual = figure.get_owner();
+
+			Assert::AreEqual(expected, actual, L"Should be player one");
+		}
+
+		TEST_METHOD(testFigureCopyConstructorGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::PAWN;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure_original(type, color, position, owner);
+
+			Figure figure_copy(figure_original);
+
+			Figure expected(figure_original);
+			Figure actual(figure_copy);
+
+			Assert::AreEqual(expected, actual, L"Should be the same figure");
+		}
+
+		TEST_METHOD(testFigureSetTypeMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+			Type type_new = Type::KING;
+			
+			figure.set_type(type_new);
+
+			Type expected = type_new;
+			Type actual = figure.get_type();
+
+			Assert::AreEqual(expected, actual, L"Should be king");
+		}
+
+		TEST_METHOD(testFigureGetTypeMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::PAWN;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+
+			Type expected = Type::PAWN;
+			Type actual = figure.get_type();
+
+			Assert::AreEqual(expected, actual, L"Should be pawn");
+		}
+
+		TEST_METHOD(testFigureSetColorMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+			Color color_new = Color::BLACK;
+			
+			figure.set_color(color_new);
+
+			Color expected = color_new;
+			Color actual = figure.get_color();
+
+			Assert::AreEqual(expected, actual, L"Should be black");
+		}
+
+		TEST_METHOD(testFigureGetColorMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+
+			Color expected = Color::WHITE;
+			Color actual = figure.get_color();
+
+			Assert::AreEqual(expected, actual, L"Should be white");
+		}
+
+		TEST_METHOD(testFigureSetPositionMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+			Position position_new(2, 2);
+
+			figure.set_position(position_new);
+
+			Position expected = position_new;
+			Position actual = figure.get_position();
+
+			Assert::AreEqual(expected, actual, L"Should be position (2, 2)");
+		}
+
+		TEST_METHOD(testFigureGetPositionMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+
+			Position expected = position;
+			Position actual = figure.get_position();
+
+			Assert::AreEqual(expected, actual, L"Should be the same position");
+		}
+
+		TEST_METHOD(testFigureSetOwnerMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+			Player owner_new = Player::PLAYER_TWO;
+
+			figure.set_owner(owner_new);
+
+			Player expected = owner_new;
+			Player actual = figure.get_owner();
+
+			Assert::AreEqual(expected, actual, L"Should be player two");
+		}
+
+		TEST_METHOD(testFigureGetOwnerMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+
+			Player expected = owner;
+			Player actual = figure.get_owner();
+
+			Assert::AreEqual(expected, actual, L"Should be player one");
+		}
+
+		TEST_METHOD(testFigureHasFigureBeenMovedMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+
+			bool expected = false;
+			bool actual = figure.has_figure_been_moved();
+
+			Assert::AreEqual(expected, actual, L"Should be false");
+		}
+
+		TEST_METHOD(testFigureHasMovedMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+
+			figure.has_moved();
+
+			bool expected = true;
+			bool actual = figure.has_figure_been_moved();
+
+			Assert::AreEqual(expected, actual, L"Should be true");
+		}
+
+		TEST_METHOD(testFigureGetSymbolMethodGivenFigurePawnWhitePositionRowFourColumnThreeOwnerPlayerOne)
+		{
+			Type type = Type::KING;
+			Color color = Color::WHITE;
+			Position position(4, 3);
+			Player owner = Player::PLAYER_ONE;
+			Figure figure(type, color, position, owner);
+
+			char expected = 'k';
+			char actual = figure.get_symbol();
+
+			Assert::AreEqual(expected, actual, L"should be k symbol for king");
 		}
 	};
 }
